c59d12a09864547c44de06b1639c254081be596c
LiShunRong <18235444249@163.com>
2017-03-21 14:13:03 +0800

    完善

--- app/src/main/java/com/example/admin/guang/VideoActivity.java
+++ app/src/main/java/com/example/admin/guang/VideoActivity.java
@@ -1,8 +1,12 @@
 package com.example.admin.guang;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.Toast;
@@ -20,6 +24,7 @@ import com.example.admin.guang.utils.VideoBean;
 @ActivityFragmentAnnoation(contentId = R.layout.activity_video)
 public class VideoActivity extends BaseActivity<ActivityVideoBinding> implements OnLoadDataFinishListener<VideoBean>, MediaPlayer.OnPreparedListener {
     String videoUrl;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void initActivityImpl() {
@@ -28,10 +33,39 @@ public class VideoActivity extends BaseActivity<ActivityVideoBinding> implements
         getWindow().setAttributes(attributes);
         videoUrl = getIntent().getStringExtra("videoUrl");
         binding.vv.setBackgroundColor(Color.BLACK);
+        gestureDetector = new GestureDetector(new SimpleListener());
         loadData();
+    }
+    class SimpleListener extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            toggleOriention();
+            return true;
+        }
+
+    }
+
+    private void toggleOriention() {
+        int requestedOrientation = getRequestedOrientation();
+        Log.e("tags",requestedOrientation+"");
+        if(requestedOrientation==ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        boolean b = gestureDetector.onTouchEvent(ev);
+        if(b){
+            return false;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
     private void loadData() {
+        binding.pb.setVisibility(View.VISIBLE);
         HttpUtils.loadVideoData(videoUrl, this);
     }
 
@@ -52,8 +86,17 @@ public class VideoActivity extends BaseActivity<ActivityVideoBinding> implements
     @Override
     public void onPrepared(MediaPlayer mp) {
         mp.start();
-
+        binding.pb.setVisibility(View.GONE);
         binding.vv.setBackgroundColor(Color.TRANSPARENT);
     }
 
+    @Override
+    public void onBackPressed() {
+        boolean b = getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+        if(b){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            return;
+        }
+        super.onBackPressed();
+    }
 }
